d9125869a0f88afbda1deb18c0059b7d1a85a403
Mas <mas@Mass-MacBook-Pro.local>
2019-04-01 12:24:20 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://samcasseusdev:fanauth@fanauth-gxtb5.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'fanauth'
+};
